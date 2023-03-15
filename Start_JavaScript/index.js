9d75b63f721886c166d5ddd06a0373c0c8535f64
Volodymyr Krolevetskyi <vvkrolevetskiy@gmail.com>
2023-03-15 22:20:19 +0200

    Start

---
+++ Start_JavaScript/index.js
@@ -0,0 +1,178 @@
+// let num=2+true;
+// console.log(num);//3
+
+
+
+// let userInput = '12345';
+// console.log(userInput);
+// let conv= Number(userInput);
+// console.log(conv);
+
+
+
+// let cat='cat';
+// console.log(cat); 
+
+// let dog='dog';
+// console.log(dog);
+
+// let result = 'cat' > 'dog';
+// console.log(result);
+
+
+
+// let appleColor= 'green';
+
+// if (appleColor==='red'){
+//     console.log('apple red');
+// } else {
+//     console.log('apple color not red');
+// }
+
+
+
+// let cat= 1;
+// console.log(cat); 
+
+// let dog= 2;
+// console.log(dog); 
+
+// let rat= 4;
+// console.log(rat);
+
+// let animal=cat+dog+rat;
+
+// console.log(animal);
+
+// if (animal>2){
+//     alert('animal');
+// }
+
+
+// let cat = 1 ; 
+// let dog = 2 ; 
+// let rat = 4 ; 
+// let animals = 0; 
+// if(cat>2){ 
+// animals += cat; 
+// } 
+// if(dog>2){ 
+// animals += dog; 
+// } 
+// if(rat>2){ 
+// animals += rat; 
+// } 
+// alert(animals);
+
+
+
+// let a = 1 ; 
+// let b = 2 ;
+// console.log(a+b);
+
+
+
+// let age=18;
+// let result;
+
+// if (age > 18){
+//     result = 'Adult';
+// } else {
+//     result = 'Not adult';
+// }
+
+//Тернарний спосіб вводу
+// let name= 'Taras';
+
+// let result= name ='Taras' ? 'Hello Taras' : 'Hello man';
+// console.log(result);
+
+//Три модальні вікна
+// alert('hello user');
+
+// let result=confirm('Are you ready to go?');
+// console.log(result);
+
+// let name=prompt('Type your name, please');
+// console.log(name);
+
+// let name=prompt('Type your name, please');
+// alert('Hello '+ name);
+
+
+// let num =Number(prompt('Type your number'));
+// console.log(num+3);
+
+
+
+//Функції
+
+// function sumTwoAndTwo(){
+//     let sum=2+2;
+//     console.log(sum);
+//     // console.log(2+2);
+// }
+
+// sumTwoAndTwo();
+
+
+// function sumTwoNumbers(arg1, arg2){
+//     let sum = arg1 + arg2;
+//     console.log(sum);
+// }
+// sumTwoNumbers(2, 3);
+// sumTwoNumbers(5, 7);
+
+// // console.log(sum);//не виводить
+
+// function something(a1, a2, a3, a4, a5){
+//     console.log(a1);
+//     console.log(a2);
+//     console.log(a3);
+//     console.log(a4);
+//     console.log(a5);
+// }
+
+// something(1, 2, 3);
+
+// function something2(){
+//     console.log('hello')
+// }
+// something2(1, 2, 3);
+
+
+// let num1 =Number(prompt('type first number'));
+// let num2 =Number(prompt('type second number'));
+
+// function getSumTwoNumber(num1, num2){
+//     let res=getSumTwoNumber(num1, num2);
+//     console.log(res);
+// }
+
+// function getBigger (num1, num2){
+//     return (num1>num2) ? num1 : num2;
+// }
+
+// function isEven(num){
+//     let rest = num % 2;
+//     if(rest===1){
+//         return fulse
+//     } else {
+//         return true
+//     }
+
+    // return (num % 2 === 0) ? true : false;
+    // return !Boolean(num % 2);
+
+// function toSquare (number){
+//     return number * number;
+// }
+//     console.log (toSquare(4));
+
+//     let userInput= prompt('Input your number');
+//     let userNumber = Number(userInput);
+
+//     console.log (toSquare(userNumber));
+
+
+
